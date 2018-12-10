cfd44cbc6fb070b65159f92edaeab71eb8ab141b
daniel vicario <danigv.email@gmail.com>
2018-12-10 13:04:02 +0100

    added unit tests

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,3 +1,47 @@
+function sum(s1, s2) {
+    if (typeof s1 === "string" || typeof s2 === "string")   throw new Error("Both params must be a number")
+    return s1 + s2
+}
+
+function employee(name, age) {
+    let salary = 40000
+
+    if (age < 20)   salary = salary / 2
+
+    return {
+        n : name,
+        a: age,
+        s: salary
+    }
+}
+
+class TogglerComponent {
+    constructor (){
+        this.state = {
+            open: false
+        }
+    }
+
+    toggle () {
+        this.state = {
+            open: !this.state.open
+        }
+    }
+
+    getState () {
+        return this.state
+    }
+}
+
+function salaryCalculator(rawSalary) {
+    return rawSalary * 1.21
+}
+
+function getDOMNode(selector) {
+    return document.querySelector(selector)
+}
+
+
 // Soldier
 function Soldier() {}
 
